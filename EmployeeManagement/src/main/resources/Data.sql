12e8094df3e3bed551281ec2d4ed5ddef975c367
RasaP <98017006+sailajaprathi4@users.noreply.github.com>
2022-07-19 08:08:00 -0400

    Add files via upload

---
+++ EmployeeManagement/src/main/resources/Data.sql
@@ -0,0 +1,6 @@
+insert into employee values(1,	 'sailaja@gmail.com',	'sailaja', 'p');
+insert into employee values(2,	 'varun@gmail.com',	'varun', 'reddy');
+insert into employee values(3,	 'Ajay@gmail.com',	'Ajay', 'raju');
+insert into employee values(4,	 'steven@gmail.com',	'sailaja', 'joy');
+insert into employee values(5,	 'kumar@gmail.com',	'kumar', 'varma');	
+insert into employee values(6,	 'yashwant@gmail.com',	'yash', 'shinde');		
