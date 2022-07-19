52c177ea3c575e8f0bdc7ddb71fd06c22bdb033e
RasaP <98017006+sailajaprathi4@users.noreply.github.com>
2022-07-19 08:01:54 -0400

    Add files via upload

---
+++ EmployeeManagement/src/main/java/com/greatlearning/employeemanagement/domain/entity/Employee.java
@@ -0,0 +1,81 @@
+package com.greatlearning.employeemanagement.domain.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+
+
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Table(name = "employee")
+public class Employee {
+
+	@Id
+	@Column(name = "employee_id")
+	private Integer id;
+	
+	@Column(name = "first_name")
+	private String firstName;
+	
+	@Column(name = "last_name")
+	private String lastName;
+	
+	@Column(name = "email")
+	private String email;
+	
+	public Employee() {
+		
+	}
+
+	public Employee(Integer id, String firstName, String lastName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+}
